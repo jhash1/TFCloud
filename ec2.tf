3322a783d3cde84f58e01911ead23548986e2612
John <john@Johns-MacBook-Pro.local>
2023-11-16 21:33:22 -0500

    fixed creds

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  region  = "us-east-1" 
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 module "ec2_instance" {
