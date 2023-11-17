e9dc5fc533ea12e1d6da070c693468dac84091a5
John <john@Johns-MacBook-Pro.local>
2023-11-17 10:54:18 -0500

    changed vars

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,16 @@
-provider "aws" {
-  region  = "us-east-1" 
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
-
 variable "AWS_SECRET_ACCESS_KEY" {
   description = "AWS Secret Key"
 }
 
 variable "AWS_ACCESS_KEY_ID" {
   description = "AWS ACCESS Key"
+}
+
+
+provider "aws" {
+  region  = "us-east-1" 
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 module "ec2_instance" {
