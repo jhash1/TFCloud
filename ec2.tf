031c2b0af3bad9c0b614caf2c8a92a1470a168e8
John <john@Johns-MacBook-Pro.local>
2023-11-18 15:28:25 -0500

    added ec2

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,5 @@
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Key"
-}
-
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS ACCESS Key"
-}
-
-
 provider "aws" {
   region  = "us-east-1" 
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 module "ec2_instance" {
